7ad890ec7bbbf9770e9819e0a80e9014bc6dc809
Searinox <searinoxx@protonmail.com>
2022-10-01 20:22:20 +0200

    changed default values for opfor distance and reduced fire building time

--- USER/userParams.hpp
+++ USER/userParams.hpp
@@ -21,7 +21,7 @@ class OpforSpawnDist
 		title = "Min distance between opfor and spike team";
 		texts[] = {"200 m","400 m","600 m","800 m","1000 m","1200 m","1400 m","1600 m","1800 m","2000 m","2200 m"};
 		values[] = {200,400,600,800,1000,1200,1400,1600,1800,2000,2200};
-		default = 1000;
+		default = 2000;
 	};	
 class OpforHintPrecision
 	{
@@ -35,7 +35,7 @@ class FireInit
 		title = "Time to build a fire or upgrade it";
 		texts[] = {"15 sek","30 sek","1 Min","1.5 Min","2 Min"};
 		values[] = {15,30,60,90,120};
-		default = 60;
+		default = 30;
 	};	
 class FireExtent
 	{
@@ -49,5 +49,5 @@ class FireExtent
 // 		title = "Burn time of a small fire";
 // 		texts[] = {"5 sek","10 sek","15 sek","30 sek","1 Min","1.5 Min","2 Min"};
 // 		values[] = {5,10,15,30,60,90,120};
-// 		default = 15;
+// 		default = 30;
 // 	};		
